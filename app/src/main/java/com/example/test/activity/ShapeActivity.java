f9edbfdee6a53af812d3716451b3fc073bfe4434
liuxiaowo <1763841230@qq.com>
2017-01-02 17:48:23 +0800

    rulerview add

--- app/src/main/java/com/example/test/activity/ShapeActivity.java
+++ app/src/main/java/com/example/test/activity/ShapeActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import com.example.physicaltests.R;
+import com.example.view.RulerView;
 
 public class ShapeActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -14,9 +15,8 @@ public class ShapeActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape);
-        //返回按钮
-        back = (ImageButton)findViewById(R.id.back_shape);
-        back.setOnClickListener(this);
+        //初始化页面
+        initView();
     }
 
     @Override
@@ -26,5 +26,32 @@ public class ShapeActivity extends AppCompatActivity implements View.OnClickList
                 finish();
                 break;
         }
+    }
+
+    /**
+     * 初始化页面
+     */
+    private void initView() {
+        //返回按钮
+        back = (ImageButton) findViewById(R.id.back_shape);
+
+        final RulerView rulerView = (RulerView) findViewById(R.id.ruler_view);
+
+        back.setOnClickListener(this);
+        //标尺带有文字
+        rulerView.isWithText();
+        //自动对齐
+        rulerView.isAutoAlign();
+        //刻度间隔
+        rulerView.setIndicatePadding(Integer.valueOf(10));
+        //初值设置
+        rulerView.smoothScrollTo(160);
+
+
+        rulerView.setOnScaleListener(new RulerView.OnScaleListener() {
+            @Override
+            public void onScaleChanged(int scale) {
+            }
+        });
     }
 }
