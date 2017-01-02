a4294400a46516bb16179de55123336836f0a9dd
liuxiaowo <1763841230@qq.com>
2017-01-02 18:23:10 +0800

    height

--- app/src/main/java/com/example/test/activity/ShapeActivity.java
+++ app/src/main/java/com/example/test/activity/ShapeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.physicaltests.R;
 import com.example.view.RulerView;
@@ -11,6 +12,8 @@ import com.example.view.RulerView;
 public class ShapeActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton back;
+    private RulerView rulerView;
+    private TextView height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,23 +37,23 @@ public class ShapeActivity extends AppCompatActivity implements View.OnClickList
     private void initView() {
         //返回按钮
         back = (ImageButton) findViewById(R.id.back_shape);
+        rulerView = (RulerView) findViewById(R.id.ruler_view);
+        height = (TextView)findViewById(R.id.shape_height);
 
-        final RulerView rulerView = (RulerView) findViewById(R.id.ruler_view);
-
-        back.setOnClickListener(this);
         //标尺带有文字
         rulerView.isWithText();
         //自动对齐
         rulerView.isAutoAlign();
         //刻度间隔
         rulerView.setIndicatePadding(Integer.valueOf(10));
-        //初值设置
-        rulerView.smoothScrollTo(160);
+        //初值设置(position-12)
+        rulerView.smoothScrollTo(138);
 
-
+        back.setOnClickListener(this);
         rulerView.setOnScaleListener(new RulerView.OnScaleListener() {
             @Override
             public void onScaleChanged(int scale) {
+                height.setText("您的身高为:"+scale+"cm");
             }
         });
     }
