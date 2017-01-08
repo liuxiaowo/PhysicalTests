3d657f9cd61677e00b3fe5f385557761b5ab8cd3
liuxiaowo <1763841230@qq.com>
2017-01-09 00:07:50 +0800

    push up test timer 2min

--- app/src/main/java/com/example/test/fragment/PushUpTestFragment.java
+++ app/src/main/java/com/example/test/fragment/PushUpTestFragment.java
@@ -7,8 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.physicaltests.R;
+import com.example.test.view.Chronometer01;
 
 /**
  *俯卧撑-自测
@@ -16,12 +18,29 @@ import com.example.physicaltests.R;
 public class PushUpTestFragment extends Fragment implements View.OnClickListener{
     //返回按钮
     private ImageButton back;
+    //倒计时
+    private Chronometer01 timer;
+    //倒计时开始
+    private ImageButton start;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_push_up_test, container, false);
         back = (ImageButton)view.findViewById(R.id.back_push_up_test);
+        timer = (Chronometer01)view.findViewById(R.id.push_up_test_timer);
+        start = (ImageButton)view.findViewById(R.id.push_up_start);
         back.setOnClickListener(this);
+        start.setOnClickListener(this);
+        timer.initTime(2*60);
+        timer.setOnTimeCompleteListener(new Chronometer01.OnTimeCompleteListener()
+        {
+            @Override
+            public void onTimeComplete()
+            {
+                Toast.makeText(getContext(), "2分钟倒计时时间到!", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
@@ -33,8 +52,13 @@ public class PushUpTestFragment extends Fragment implements View.OnClickListener
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+            //返回按钮
             case R.id.back_push_up_test:
                 getActivity().finish();
+                break;
+            //倒计时重置按钮
+            case R.id.push_up_start:
+                timer.reStart();
                 break;
         }
     }
