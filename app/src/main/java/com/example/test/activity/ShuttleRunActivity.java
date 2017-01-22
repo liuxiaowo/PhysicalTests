8814c1c5eeb94b7889f46a162296e5250baf3094
liuxiaowo <1763841230@qq.com>
2017-01-22 16:08:06 +0800

    ShuttleRunActivity.java change

--- app/src/main/java/com/example/test/activity/ShuttleRunActivity.java
+++ app/src/main/java/com/example/test/activity/ShuttleRunActivity.java
@@ -2,17 +2,26 @@ package com.example.test.activity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 
 import com.example.physicaltests.R;
 
 public class ShuttleRunActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //返回按钮
     private ImageButton back;
+    //计时
+    private Chronometer timer;
+    //按钮
+    private Button start,end,reset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,15 @@ public class ShuttleRunActivity extends AppCompatActivity implements View.OnClic
         switch(v.getId()){
             case R.id.back_shuttle_run:
                 finish();
+                break;
+            case R.id.btnStart:
+                timer.start();
+                break;
+            case R.id.btnStop:
+                timer.stop();
+                break;
+            case R.id.btnReset:
+                timer.setBase(SystemClock.elapsedRealtime());// 复位
                 break;
         }
     }
@@ -42,6 +60,13 @@ public class ShuttleRunActivity extends AppCompatActivity implements View.OnClic
         }
         //返回按钮
         back = (ImageButton)findViewById(R.id.back_shuttle_run);
+        timer = (Chronometer)findViewById(R.id.shuttle_run_timer);
+        start = (Button)findViewById(R.id.btnStart);
+        end = (Button)findViewById(R.id.btnStop);
+        reset = (Button)findViewById(R.id.btnReset);
         back.setOnClickListener(this);
+        start.setOnClickListener(this);
+        end.setOnClickListener(this);
+        reset.setOnClickListener(this);
     }
 }
